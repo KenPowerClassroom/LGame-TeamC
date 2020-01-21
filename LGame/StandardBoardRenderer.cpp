dc8794d20633131b4110760b1cddff4fc2555dde
(Student) - Denis Bastien Becle <C00254311@itcarlow.ie>
2020-01-21 17:34:38 +0000

    Empty board displayed

--- LGame/StandardBoardRenderer.cpp
+++ LGame/StandardBoardRenderer.cpp
@@ -7,4 +7,17 @@ StandardBoardRenderer::StandardBoardRenderer(std::ostream& output)
 
 void StandardBoardRenderer::renderBoard(int boardArray[4][4])
 {
+	std::string displayString;
+
+	m_output << "   A B C D";
+	for (int i = 0; i < 4; i++)
+	{
+		m_output << "\n" << std::to_string(i+1) << ":";
+		for (int i = 0; i < 4; i++)
+		{
+			m_output << " ";
+			m_output << "0";
+		}
+	}
+
 }
--- LGame/StandardBoardRenderer.h
+++ LGame/StandardBoardRenderer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include "BoardRenderer.h"
 
 class StandardBoardRenderer : public BoardRenderer
