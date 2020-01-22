5f8291eac2d354af4f1349aac5384d5a1f58f9e6
(Student) - Denis Bastien Becle <C00254311@itcarlow.ie>
2020-01-22 13:19:18 +0000

    Display Board : display exemple

--- LGame/LGame.cpp
+++ LGame/LGame.cpp
@@ -6,7 +6,14 @@
 
 int main()
 {
-
+	int board[4][4]
+		= { {3,1,1,0},
+			{0,2,1,0},
+			{0,2,1,0},
+			{0,2,2,4}
+	};
+	StandardBoardRenderer renderer(std::cout);
+	renderer.renderBoard(board);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
