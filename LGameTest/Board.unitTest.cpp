62a390ef4c59752b1e563d972ede9b17050744b2
(Student) - Denis Bastien Becle <c00254311@ITCARLOW.IE>
2020-02-19 14:07:09 +0000

    LPieceMovement : More equality tests added

--- LGameTest/Board.unitTest.cpp
+++ LGameTest/Board.unitTest.cpp
@@ -15,3 +15,83 @@ TEST(Board, equalityTest) {
 	Board b2(nullptr, data);
 	ASSERT_TRUE(b1 == b2);
 }
+
+TEST(Board, inequalityTest) {
+	int data[4][4]
+		= { {0,0,0,0},
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,0,0,0}
+	};
+
+	int data2[4][4]
+		= { {1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12},
+			{13,14,15,16}
+	};
+
+	Board b1(nullptr, data);
+	Board b2(nullptr, data2);
+	ASSERT_FALSE(b1 == b2);
+}
+
+TEST(Board, equalityTest2) {
+	int data[4][4]
+		= { {1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12},
+			{13,14,15,16}
+	};
+
+	int data2[4][4]
+		= { {1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12},
+			{13,14,15,16}
+	};
+
+	Board b1(nullptr, data);
+	Board b2(nullptr, data2);
+	ASSERT_TRUE(b1 == b2);
+}
+
+TEST(Board, equalityTest3) {
+	int data[4][4]
+		= { {0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,1},
+			{2,2,2,2}
+	};
+
+	int data2[4][4]
+		= { {0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,1},
+			{2,2,2,2}
+	};
+
+	Board b1(nullptr, data);
+	Board b2(nullptr, data2);
+	ASSERT_TRUE(b1 == b2);
+}
+
+TEST(Board, inequalityTest2) {
+	int data[4][4]
+		= { {0,0,0,0},
+			{0,1,0,0},
+			{0,0,0,0},
+			{0,0,0,0}
+	};
+
+	int data2[4][4]
+		= { {0,0,0,0},
+			{0,0,0,0},
+			{0,0,0,0},
+			{0,0,0,0}
+	};
+
+	Board b1(nullptr, data);
+	Board b2(nullptr, data2);
+	ASSERT_FALSE(b1 == b2);
+}
